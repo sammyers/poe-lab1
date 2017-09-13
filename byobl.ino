a7e5a25b091348016c9445490ad11be25ac5e6e1
lnielsen <louiseenielsen@gmail.com>
2017-09-13 14:43:21 -0400

    comments, whee

--- byobl.ino
+++ byobl.ino
@@ -1,16 +1,21 @@
+// Define pins
 const int LED_PINS[] = { 11, 12, 13 };
 const int BUTTON_PIN = 10;
+const int SENSOR_PIN = A0;
 
-int blinkInterval = 100;
+// Initialize state variables
 int ledStates[] = { LOW, LOW, LOW };
 int lightMode = 0;
 int buttonState = 0;
 
+// Blinking and potentiometering state variables
 unsigned long previousMillis = 0;
 int blinkState = 0;
 int bounceState = 0;
+int blinkInterval = 100; // ms
+int sensorValue = 0;
 
-// Morse Code Constants
+// Morse code constants
 int dotInterval = 100;
 int dashInterval = 3 * dotInterval;
 int elementPauseInterval = dotInterval;
@@ -23,10 +28,7 @@ boolean lightOn = false;
 int sosSignals[] = { 1, 1, 1, 0, 2, 2, 2, 0, 1, 1, 1, 3 };
 int sosSignalLength = sizeof(sosSignals) / sizeof(int);
 
-// Potentiometer ints
-const int SENSOR_PIN = A0;
-int sensorValue = 0;
-
+// Helper functions
 void incrementValue(int *value, int modulus) {
   *value = (*value + 1) % modulus;
 }
@@ -41,6 +43,12 @@ void writeToAllLeds(int val) {
   }
 }
 
+/*
+ * Calls provided function with the provided `data` argument if a given
+ * interval (in milliseconds) has elapsed.
+ *
+ * Used for blink, bounce, and morse code modes.
+ */
 void executeIfTimeElapsed(void (*callback)(void *), int interval, void *data) {
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
@@ -49,6 +57,7 @@ void executeIfTimeElapsed(void (*callback)(void *), int interval, void *data) {
   }
 }
 
+// Wrapper functions to turn on and off all LEDs
 void allOn() {
   writeToAllLeds(HIGH);
 }
@@ -57,6 +66,7 @@ void allOff() {
   writeToAllLeds(LOW);
 }
 
+// Toggles all lights on/off
 void blinkCallback() {
   if (blinkState == LOW) {
     blinkState = HIGH;
@@ -66,14 +76,17 @@ void blinkCallback() {
   writeToAllLeds(blinkState);
 }
 
+// Runs in main loop; toggles lights when blink intervals have elapsed
 void blink() {
   executeIfTimeElapsed(blinkCallback, blinkInterval, NULL);
 }
 
+// bounceState keeps track of which LED is lit
 void incrementBounceState() {
   incrementValue(&bounceState, 3);
 }
 
+// Toggles current LED off and next LED on
 void bounceCallback() {
   for (int i = 0; i < 3; i++) {
     if (i == bounceState) {
@@ -85,14 +98,20 @@ void bounceCallback() {
   incrementBounceState();
 }
 
+// Runs in main loop; handles light toggling when intervals have elapsed
 void bounce() {
   executeIfTimeElapsed(bounceCallback, blinkInterval, NULL);
 }
 
+// Increments through array of morse code elements
 void incrementMorseCodeElement(void *arrayLength) {
   incrementValue(&signalIndex, *((int *) arrayLength));
 }
 
+/*
+ * Wrapper functions for allOn and allOff,
+ * update morse code state variables accordingly
+ */
 void turnMorseCodeLightOn() {
   allOn();
   lightOn = true;
@@ -104,6 +123,10 @@ void turnMorseCodeLightOff(void *signalLength) {
   incrementMorseCodeElement(signalLength);
 }
 
+/*
+ * Takes in array of signals and length of array. Toggles all lights on/off
+ * based on provided signals and current time.
+ */
 void morseCode(int *signals, int signalLength) {
   int signal = signals[signalIndex];
   int prevSignal = signalLength - 1;
@@ -145,12 +168,13 @@ void morseCode(int *signals, int signalLength) {
   }
 }
 
+// Wrapper function to blink SOS in morse code
 void sos() {
   morseCode(sosSignals, sosSignalLength);
 }
 
+// Sets things up (initializes input and output pins)
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   for (int i = 0; i < 3; i++) {
     pinMode(LED_PINS[i], OUTPUT);
@@ -158,8 +182,14 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT);
 }
 
+/*
+ * Runs until program terminates.
+ *
+ * Keeps track of current potentiometer value and updates blinkInterval.
+ *
+ * Keeps track of current button state and switches between modes.
+ */
 void loop() {
-  // put your main code here, to run repeatedly:
   sensorValue = analogRead(SENSOR_PIN);
   blinkInterval = 3 * sensorValue + 100;
   int currentButtonState = digitalRead(BUTTON_PIN);
@@ -188,5 +218,3 @@ void loop() {
       break;
   }
 }
-
-
